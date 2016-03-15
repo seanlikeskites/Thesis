d35db3f53eb953461a8f562e546575dfc41823f1
Sean Enderby <sean.enderby@gmail.com>
2016-03-15 12:02:57 +0000

    and the toil of the peons was over

--- RPlots/distortionPlots.r
+++ RPlots/distortionPlots.r
@@ -1,28 +1,22 @@
 library(SAFER)
 
-features <- c(SpectralFeatures, 30, 38:42)
 descriptorsToPlot <- c("warm", "bright", "fuzz", "fuzzy", "crunch", "crunchy", "raspy", "harsh")
-
-processedMDS <- individualEntryFeatureSpace ("SAFEDistortion", features, "Processed", "MDS")
-processedTSNE <- individualEntryFeatureSpace ("SAFEDistortion", features, "Processed", "TSNE")
-
-differenceMDS <- individualEntryFeatureSpace ("SAFEDistortion", features, "Differences", "MDS")
-differenceTSNE <- individualEntryFeatureSpace ("SAFEDistortion", features, "Differences", "TSNE")
+load("distortionData.RData")
 
 setEPS()
 
 postscript("DistortionProcessedMDS.eps")
-plotTimbreSpace(processedMDS, descriptorsToPlot, "Pretty", FALSE)
+a <- plotTimbreSpace(processedMDS, descriptorsToPlot, "Pretty", TRUE, FALSE)
 dev.off()
 
 postscript("DistortionProcessedTSNE.eps")
-plotTimbreSpace(processedTSNE, descriptorsToPlot, "Pretty", FALSE)
+plotTimbreSpace(processedTSNE, descriptorsToPlot, "Pretty", TRUE, FALSE)
 dev.off()
 
 postscript("DistortionDifferenceMDS.eps")
-plotTimbreSpace(differenceMDS, descriptorsToPlot, "Pretty", FALSE)
+plotTimbreSpace(differenceMDS, descriptorsToPlot, "Pretty", TRUE, FALSE)
 dev.off()
 
 postscript("DistortionDifferenceTSNE.eps")
-plotTimbreSpace(differenceTSNE, descriptorsToPlot, "Pretty", FALSE)
+plotTimbreSpace(differenceTSNE, descriptorsToPlot, "Pretty", TRUE, FALSE)
 dev.off()
--- RPlots/equaliserPlots.r
+++ RPlots/equaliserPlots.r
@@ -1,18 +1,12 @@
 library(SAFER)
 
-features <- c(SpectralFeatures, 30, 38:42)
 descriptorsToPlot <- c("air", "airy", "warm", "bright", "harsh", "sharp", "clear", "full")
-
-processedMDS <- individualEntryFeatureSpace ("SAFEEqualiser", features, "Processed", "MDS")
-#processedTSNE <- individualEntryFeatureSpace ("SAFEEqualiser", features, "Processed", "TSNE")
-
-differenceMDS <- individualEntryFeatureSpace ("SAFEEqualiser", features, "Differences", "MDS")
-#differenceTSNE <- individualEntryFeatureSpace ("SAFEEqualiser", features, "Differences", "TSNE")
+load("equaliserData.RData")
 
 setEPS()
 
 postscript("EqualiserProcessedMDS.eps")
-plotTimbreSpace(processedMDS, descriptorsToPlot, "Pretty", FALSE)
+plotTimbreSpace(processedMDS, descriptorsToPlot, "Pretty", TRUE, FALSE)
 dev.off()
 
 #postscript("EqualiserProcessedTSNE.eps")
@@ -20,7 +14,7 @@ dev.off()
 #dev.off()
 
 postscript("EqualiserDifferenceMDS.eps")
-plotTimbreSpace(differenceMDS, descriptorsToPlot, "Pretty", FALSE)
+plotTimbreSpace(differenceMDS, descriptorsToPlot, "Pretty", TRUE, FALSE)
 dev.off()
 
 #postscript("EqualiserDifferenceTSNE.eps")
