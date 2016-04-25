5527f1654851a7b3ffa9bdff8fa41e3a79e7b2ec
Sean Enderby <sean.enderby@gmail.com>
2016-04-25 13:47:49 +0100

    dendrograms

--- RPlots/distortionClusters.r
+++ RPlots/distortionClusters.r
@@ -1,4 +1,13 @@
 library(SnowballC)
+library(SAFER)
+library(dendextend)
+
+makePrettyDendrogram <- function(clusters, numColours)
+{
+	dend <- as.dendrogram(clusters)
+	dend <- color_branches(dend, k=numColours)
+	dend <- hang.dendrogram(dend, hang_height=0.1)
+}
 
 load("distortionData.RData")
 
@@ -7,17 +16,19 @@ setEPS()
 processedFeatures <- processedMDS$Features
 rownames(processedFeatures) <- sub("i$", "", wordStem(rownames(processedFeatures)))
 processedAverages <- apply(processedFeatures, 2, function(x) tapply(x, rownames(processedFeatures), mean))
-processedClusters <- hclust(dist(processedAverages))
+processedClusters <- hclust(dist(normalise(processedAverages)))
+processedDend <- makePrettyDendrogram(processedClusters, 3)
 
 postscript("DistortionProcessedClusters.eps")
-a <- plot(processedClusters, main=NA, sub=NA, xlab=NA, ylab=NA, xaxt="n", yaxt="n")
+a <- plot(processedDend, main=NA, sub=NA, xlab=NA, ylab=NA, horiz=TRUE)
 dev.off()
 
 differenceFeatures <- differenceMDS$Features
 rownames(differenceFeatures) <- sub("i$", "", wordStem(rownames(differenceFeatures)))
 differenceAverages <- apply(differenceFeatures, 2, function(x) tapply(x, rownames(differenceFeatures), mean))
-differenceClusters <- hclust(dist(differenceAverages))
+differenceClusters <- hclust(dist(normalise(differenceAverages)))
+differenceDend <- makePrettyDendrogram(differenceClusters, 3)
 
 postscript("DistortionDifferenceClusters.eps")
-a <- plot(differenceClusters, main=NA, sub=NA, xlab=NA, ylab=NA, xaxt="n", yaxt="n")
+a <- plot(differenceDend, main=NA, sub=NA, xlab=NA, ylab=NA, horiz=TRUE)
 dev.off()
