808cff44a9b90d913a5bcea991aca8a9442026bd
Sean Enderby <sean.enderby@gmail.com>
2016-08-10 20:46:00 +0100

    and he did see fit to strike them down

--- RPlots/FeatureManipulation/plotManipulations.r
+++ RPlots/FeatureManipulation/plotManipulations.r
@@ -7,7 +7,7 @@ plotManipulation <- function(x, y, lims, legendText, legendPos, xlab, ylab)
 {
 	nLines <- nrow(y)
 	colours <- rainbow(nLines)
-	par(xaxs='i', yaxs='i', mar=c(4, 4.5, 0.5, 0.5))
+	par(xaxs='i', yaxs='i', mar=c(4, 4.5, 0.6, 4.5))
 	plot(x, y[1,], type='n', main="", xlab=xlab, ylab=ylab, xlim=c(lims[1], lims[2]), ylim=c(lims[3], lims[4]))
 
 	for (i in 1:nLines)
@@ -24,7 +24,7 @@ plotManipulation <- function(x, y, lims, legendText, legendPos, xlab, ylab)
 ########################################################
 gains = seq(0, 1, 0.01)
 centroids <- read.octave("MoveCentroids.mat")$centroids[c(1, 2, 4, 3),] / 1000
-pdf("MoveCentroids.pdf", pointsize=8, family="CM Sans", width=4.2, height=3)
+pdf("MoveCentroids.pdf", pointsize=9, family="CM Sans", width=4.2, height=3)
 plotManipulation(gains, centroids, c(0, 1, 0, 6), signals, "topleft", "Parameter Setting", "Spectral Centroid (kHz)")
 dev.off()
 embed_fonts("MoveCentroids.pdf")
@@ -34,7 +34,7 @@ embed_fonts("MoveCentroids.pdf")
 ########################################################
 gains = seq(0, 10, 0.1)
 spreads <- read.octave("MoveSpreads.mat")$spreads[c(1, 2, 4, 3),] / 1000000
-pdf("MoveSpreads.pdf", pointsize=8, family="CM Sans", width=4.2, height=3)
+pdf("MoveSpreads.pdf", pointsize=9, family="CM Sans", width=4.2, height=3)
 plotManipulation(gains, spreads, c(0, 10, 4, 16), signals, "topright", "Band Gain",
 		 expression("Spectral Spread" ~ (kHz^2)))
 dev.off()
@@ -45,7 +45,7 @@ embed_fonts("MoveSpreads.pdf")
 ########################################################
 gains = seq(-10, 10, 0.5)
 skews <- read.octave("MoveSkewnesses.mat")$skews[c(1, 2, 4, 3),]
-pdf("MoveSkewnesses.pdf", pointsize=8, family="CM Sans", width=4.2, height=3)
+pdf("MoveSkewnesses.pdf", pointsize=9, family="CM Sans", width=4.2, height=3)
 plotManipulation(gains, skews, c(-10, 10, 0, 25), signals, "topright", "Tilt Gradient", "Spectral Skewness")
 dev.off()
 embed_fonts("MoveSkewnesses.pdf")
@@ -55,7 +55,34 @@ embed_fonts("MoveSkewnesses.pdf")
 ########################################################
 gains = seq(0, 10, 0.1)
 kurts <- read.octave("MoveKurtoses.mat")$kurts[c(1, 2, 4, 3),]
-pdf("MoveKurtoses.pdf", pointsize=8, family="CM Sans", width=4.2, height=3)
+pdf("MoveKurtoses.pdf", pointsize=9, family="CM Sans", width=4.2, height=3)
 plotManipulation(gains, kurts, c(0, 10, 6, 16), signals, "topleft", "Band Gain", "Spectral Kurtosis")
 dev.off()
 embed_fonts("MoveKurtoses.pdf")
+
+########################################################
+# irregularities
+########################################################
+m = seq(-1, 1, 0.01)
+irregs <- read.octave("MoveIrregularities.mat")$irregs
+irregKs <- t(irregs[1,,c(1, 2, 4, 3)])
+irregJs <- t(irregs[2,,c(1, 2, 4, 3)])
+irregBs <- t(irregs[3,,c(1, 2, 4, 3)])
+
+pdf("MoveIrregularitiesK.pdf", pointsize=9, family="CM Sans", width=4.2, height=3)
+plotManipulation(m, irregKs, c(-1, 1, 0, 0.4), signals, "topright", "Parameter Setting",
+		 "Krimphoff Irregularity")
+dev.off()
+embed_fonts("MoveIrregularitiesK.pdf")
+
+pdf("MoveIrregularitiesJ.pdf", pointsize=9, family="CM Sans", width=4.2, height=3)
+plotManipulation(m, irregJs, c(-1, 1, 0, 1), signals, "bottomleft", "Parameter Setting",
+		 "Jensen Irregularity")
+dev.off()
+embed_fonts("MoveIrregularitiesJ.pdf")
+
+pdf("MoveIrregularitiesB.pdf", pointsize=9, family="CM Sans", width=4.2, height=3)
+plotManipulation(m, irregBs, c(-1, 1, 0, 0.4), signals, "topright", "Parameter Setting",
+		 "Beauchamp Irregularity")
+dev.off()
+embed_fonts("MoveIrregularitiesB.pdf")
