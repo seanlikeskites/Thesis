7ac3af97288a81a7e3585531a1279bac900de9ab
Sean Enderby <sean.enderby@gmail.com>
2016-10-24 12:48:57 +0100

    and they found that the masses were not in fact deliverant upon their promises

---
+++ RPlots/Validation/analyseResults.r
@@ -0,0 +1,18 @@
+doods <- dir("results")
+
+harsh <- data.frame()
+crunch <- data.frame()
+
+for (dood in doods)
+{
+	harshFile <- paste("results/", dood, "/HarshResults.txt", sep="")
+	tempHarsh <- read.csv(harshFile, header=FALSE)
+	harsh <- rbind(harsh, tempHarsh)
+
+	crunchFile <- paste("results/", dood, "/CrunchResults.txt", sep="")
+	tempCrunch <- read.csv(crunchFile, header=FALSE)
+	crunch <- rbind(crunch, tempCrunch)
+}
+
+harsh <- harsh[order(harsh$V1),] 
+crunch <- crunch[order(crunch$V1),]
