79419574afdef6e9c5b07bc7dae9a9b8d4176af4
Sean Enderby <sean.enderby@gmail.com>
2016-10-31 09:54:34 +0000

    and they found their houses in ruin

--- RPlots/Validation/analyseResults.r
+++ RPlots/Validation/analyseResults.r
@@ -1,6 +1,29 @@
 source("../SAFEAnalysis/stemming.r")
 load("../SAFEAnalysis/ClusterDistances.RData")
 
+prependDescriptors <- function(descriptors, cols)
+{
+	distDescriptors <- c("crunch", "fuzz", "cream", "rasp", "smooth", "harsh")
+	eqDescriptors <- c("clear", "air", "thin", "full", "boom", "box", "tin", "deep", "mud")
+	sharedDescriptors <- c("warm", "bright")
+
+	distIdx <- descriptors %in% distDescriptors
+	descriptors[distIdx] <- paste("D:", descriptors[distIdx], sep="")
+
+	eqIdx <- descriptors %in% eqDescriptors
+	descriptors[eqIdx] <- paste("E:", descriptors[eqIdx], sep="")
+
+	special <- descriptors[,cols]
+	specialIdx <- special %in% sharedDescriptors
+	special[specialIdx] <- paste("E:", special[specialIdx], sep="")
+	descriptors[,cols] <- special
+
+	sharedIdx <- descriptors %in% sharedDescriptors
+	descriptors[sharedIdx] <- paste("D:", descriptors[sharedIdx], sep="")
+
+	return(descriptors)
+}
+
 doods <- dir("results")
 
 harsh <- data.frame()
@@ -22,3 +45,6 @@ crunch <- trimws(as.matrix(crunch[order(crunch$V1),]))
 
 harsh[,2:4] <- safeStem(harsh[,2:4])
 crunch[,2:4] <- safeStem(crunch[,2:4])
+
+harsh <- prependDescriptors(harsh, 2)
+crunch <- prependDescriptors(crunch, c())
