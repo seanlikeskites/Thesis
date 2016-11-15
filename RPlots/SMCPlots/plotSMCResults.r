b09a0d930b6b1d2032883038e25db7bc6ed836ee
Sean Enderby <sean.enderby@gmail.com>
2016-11-15 09:53:35 +0000

    and the parted seas crashed closed

--- RPlots/SMCPlots/plotSMCResults.r
+++ RPlots/SMCPlots/plotSMCResults.r
@@ -4,8 +4,10 @@ library(extrafont)
 plotResults <- function(res, conf)
 {
 	par(lwd=0.5)
-	centres <- barplot(res, names.arg=as.character(1:11), xlab="Stimulus Number", ylab="Normalised Grade",
+	centres <- barplot(res, ylab="Normalised Grade",
 			   col="blue")
+	axis(1, at=centres, line=-1, lwd=0, labels=as.character(1:11))
+	mtext("Stimulus Number", 1, 2)
 
 	# find which error bars we should draw
 	arrowsToDraw <- conf != 0
@@ -26,25 +28,25 @@ results <- readMat("MushraResults.mat")
 confs <- results$res - results$conf[,,1]
 
 pdf("CelloResults.pdf", pointsize=8, family="CM Sans", width=2.94, height=2.1)
-par(mar=c(4, 4, 0.8, 0))
+par(mar=c(3, 4, 0.8, 0))
 plotResults(results$res[1,], confs[1,])
 dev.off()
 embed_fonts("CelloResults.pdf")
 
 pdf("SynthResults.pdf", pointsize=8, family="CM Sans", width=2.94, height=2.1)
-par(mar=c(4, 4, 0.8, 0))
+par(mar=c(3, 4, 0.8, 0))
 plotResults(results$res[2,], confs[2,])
 dev.off()
 embed_fonts("SynthResults.pdf")
 
 pdf("PianoResults.pdf", pointsize=8, family="CM Sans", width=2.94, height=2.1)
-par(mar=c(4, 4, 0.8, 0))
+par(mar=c(3, 4, 0.8, 0))
 plotResults(results$res[3,], confs[3,])
 dev.off()
 embed_fonts("PianoResults.pdf")
 
 pdf("ClarinetResults.pdf", pointsize=8, family="CM Sans", width=2.94, height=2.1)
-par(mar=c(4, 4, 0.8, 0))
+par(mar=c(3, 4, 0.8, 0))
 plotResults(results$res[4,], confs[4,])
 dev.off()
 embed_fonts("ClarinetResults.pdf")
@@ -53,26 +55,25 @@ embed_fonts("ClarinetResults.pdf")
 rnonlin <- readMat("RNonlin_Results.mat")
 
 pdf("CelloRNonlin.pdf", pointsize=8, family="CM Sans", width=2.94, height=2.1)
-par(mar=c(4, 4, 0.8, 0))
+par(mar=c(3, 4, 0.8, 0))
 plotResults(rnonlin$results[1,], array(0, 11))
 dev.off()
 embed_fonts("CelloRNonlin.pdf")
 
 pdf("SynthRNonlin.pdf", pointsize=8, family="CM Sans", width=2.94, height=2.1)
-par(mar=c(4, 4, 0.8, 0))
+par(mar=c(3, 4, 0.8, 0))
 plotResults(rnonlin$results[2,], array(0, 11))
 dev.off()
 embed_fonts("SynthRNonlin.pdf")
 
 pdf("PianoRNonlin.pdf", pointsize=8, family="CM Sans", width=2.94, height=2.1)
-par(mar=c(4, 4, 0.8, 0))
+par(mar=c(3, 4, 0.8, 0))
 plotResults(rnonlin$results[3,], array(0, 11))
 dev.off()
 embed_fonts("PianoRNonlin.pdf")
 
 pdf("ClarinetRNonlin.pdf", pointsize=8, family="CM Sans", width=2.94, height=2.1)
-par(mar=c(4, 4, 0.8, 0))
+par(mar=c(3, 4, 0.8, 0))
 plotResults(rnonlin$results[4,], array(0, 11))
 dev.off()
 embed_fonts("ClarinetRNonlin.pdf")
-
