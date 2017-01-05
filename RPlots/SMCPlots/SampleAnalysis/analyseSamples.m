644ab250f054c71a95bc7f787c22ccd417e27cac
Sean Enderby <sean.enderby@gmail.com>
2017-01-05 15:32:53 +0000

    and they build houses with greater viesws

--- RPlots/SMCPlots/SampleAnalysis/analyseSamples.m
+++ RPlots/SMCPlots/SampleAnalysis/analyseSamples.m
@@ -1,6 +1,15 @@
 load("samples.mat")
 
-[bassAmps, bassFreqs] = getHarmonicRelationships (bass, bassFs, bassF0);
-[clarAmps, clarFreqs] = getHarmonicRelationships (clar, clarFs, clarF0);
-[pianAmps, pianFreqs] = getHarmonicRelationships (pian, pianFs, pianF0);
-[synAmps, synFreqs] = getHarmonicRelationships (syn, synFs, synF0);
+windowSize = 4096;
+
+[bassAmps, bassFreqs] = getHarmonicRelationships (bass, bassFs, bassF0, windowSize);
+bassAmpRatios = bassAmps ./ repmat(bassAmps(1, :), 9, 1);
+
+[clarAmps, clarFreqs] = getHarmonicRelationships (clar, clarFs, clarF0, windowSize);
+clarAmpRatios = clarAmps ./ repmat(clarAmps(1, :), 9, 1);
+
+[pianAmps, pianFreqs] = getHarmonicRelationships (pian, pianFs, pianF0, windowSize);
+pianAmpRatios = pianAmps ./ repmat(pianAmps(1, :), 9, 1);
+
+[synAmps, synFreqs] = getHarmonicRelationships (syn, synFs, synF0, windowSize);
+synAmpRatios = synAmps ./ repmat(synAmps(1, :), 9, 1);
