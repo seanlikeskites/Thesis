fca0f70fb124f03466377d0127b0de22a8fc063f
Sean Enderby <sean.enderby@gmail.com>
2017-01-19 15:53:02 +0000

    and the trolls of the valley rose up

--- RPlots/SMCPlots/SampleAnalysis/analyseSamples.m
+++ RPlots/SMCPlots/SampleAnalysis/analyseSamples.m
@@ -11,6 +11,18 @@ bassFreqRatios = bassFreqs ./ repmat(bassFreqs(1, :), 9, 1);
 clarAmpRatios = clarAmps ./ repmat(clarAmps(1, :), 9, 1);
 clarFreqRatios = clarFreqs ./ repmat(clarFreqs(1, :), 9, 1);
 
+[clarSTFT50Amps, clarSTFT50Freqs] = getHarmonicRelationships (clarSTFT50, clarFs, clarF0, windowSize, stepSize);
+clarSTFT50AmpRatios = clarSTFT50Amps ./ repmat(clarSTFT50Amps(1, :), 9, 1);
+clarSTFT50FreqRatios = clarSTFT50Freqs ./ repmat(clarSTFT50Freqs(1, :), 9, 1);
+
+[clarSTFT100Amps, clarSTFT100Freqs] = getHarmonicRelationships (clarSTFT100, clarFs, clarF0, windowSize, stepSize);
+clarSTFT100AmpRatios = clarSTFT100Amps ./ repmat(clarSTFT100Amps(1, :), 9, 1);
+clarSTFT100FreqRatios = clarSTFT100Freqs ./ repmat(clarSTFT100Freqs(1, :), 9, 1);
+
+[clarSTFT500Amps, clarSTFT500Freqs] = getHarmonicRelationships (clarSTFT500, clarFs, clarF0, windowSize, stepSize);
+clarSTFT500AmpRatios = clarSTFT500Amps ./ repmat(clarSTFT500Amps(1, :), 9, 1);
+clarSTFT500FreqRatios = clarSTFT500Freqs ./ repmat(clarSTFT500Freqs(1, :), 9, 1);
+
 [pianAmps, pianFreqs] = getHarmonicRelationships (pian, pianFs, pianF0, windowSize, stepSize);
 pianAmpRatios = pianAmps ./ repmat(pianAmps(1, :), 9, 1);
 pianFreqRatios = pianFreqs ./ repmat(pianFreqs(1, :), 9, 1);
@@ -22,6 +34,9 @@ synFreqRatios = synFreqs ./ repmat(synFreqs(1, :), 9, 1);
 save("HarmonicRelationships.mat",
      "bassAmpRatios", "bassFreqRatios",
      "clarAmpRatios", "clarFreqRatios",
+     "clarSTFT50AmpRatios", "clarSTFT50FreqRatios",
+     "clarSTFT100AmpRatios", "clarSTFT100FreqRatios",
+     "clarSTFT500AmpRatios", "clarSTFT500FreqRatios",
      "pianAmpRatios", "pianFreqRatios",
      "synAmpRatios", "synFreqRatios",
      "stepSize")
--- RPlots/SMCPlots/SampleAnalysis/readSamples.m
+++ RPlots/SMCPlots/SampleAnalysis/readSamples.m
@@ -2,6 +2,9 @@
 bassF0 = 221.58714;
 
 [clar, clarFs] = wavread("clarinet.wav");
+clarSTFT50 = wavread("clarinetSTFT50.wav");
+clarSTFT100 = wavread("clarinetSTFT100.wav");
+clarSTFT500 = wavread("clarinetSTFT500.wav");
 clarF0 = 195.75648;
 
 [pian, pianFs] = wavread("piano.wav");
@@ -13,5 +16,6 @@ synF0 = 55.235178;
 save("samples.mat", 
      "bass", "bassFs", "bassF0",
      "clar", "clarFs", "clarF0",
+     "clarSTFT50", "clarSTFT100", "clarSTFT500",
      "pian", "pianFs", "pianF0",
      "syn", "synFs", "synF0")
