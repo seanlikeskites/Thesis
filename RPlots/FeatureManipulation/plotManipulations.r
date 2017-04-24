9eaf61d6c35f2db0776ba65333e0053ad1a04f43
Sean Enderby <sean.enderby@gmail.com>
2017-04-24 12:25:55 +0100

    and the script did bely their evey whim

--- RPlots/FeatureManipulation/plotManipulations.r
+++ RPlots/FeatureManipulation/plotManipulations.r
@@ -48,9 +48,10 @@ embed_fonts("MoveCentroids.pdf")
 ########################################################
 gains <- seq(0, 10, 0.1)
 spreads <- read.octave("MoveSpreads.mat")$spreads[c(1, 2, 4, 3),] / 1000000
-pdf("MoveSpreads.pdf", pointsize=9, family="CM Sans", width=4.2, height=3)
-plotManipulation(gains, spreads, c(0, 10, 4, 16), signals, "topright", "Band Gain",
+pdf("MoveSpreads.pdf", pointsize=9, fonts=c("CM Roman", "CM Sans"), family="CM Sans", width=4.2, height=3)
+plotManipulation(gains, spreads, c(0, 10, 4, 16), signals, "topright", "",
 		 expression("Spectral Spread" ~ (kHz^2)), bigMar)
+mtext(expression(italic(m)), side=1, line=3, family="CM Roman")
 dev.off()
 embed_fonts("MoveSpreads.pdf")
 
@@ -59,8 +60,9 @@ embed_fonts("MoveSpreads.pdf")
 ########################################################
 gains <- seq(-10, 10, 0.5)
 skews <- read.octave("MoveSkewnesses.mat")$skews[c(1, 2, 4, 3),]
-pdf("MoveSkewnesses.pdf", pointsize=9, family="CM Sans", width=4.2, height=3)
-plotManipulation(gains, skews, c(-10, 10, 0, 25), signals, "topright", "Tilt Gradient", "Spectral Skewness", bigMar)
+pdf("MoveSkewnesses.pdf", pointsize=9, fonts=c("CM Roman", "CM Sans"), family="CM Sans", width=4.2, height=3)
+plotManipulation(gains, skews, c(-10, 10, 0, 25), signals, "topright", "", "Spectral Skewness", bigMar)
+mtext(expression(italic(k)), side=1, line=3, family="CM Roman")
 dev.off()
 embed_fonts("MoveSkewnesses.pdf")
 
@@ -69,8 +71,9 @@ embed_fonts("MoveSkewnesses.pdf")
 ########################################################
 gains <- seq(0, 10, 0.1)
 kurts <- read.octave("MoveKurtoses.mat")$kurts[c(1, 2, 4, 3),]
-pdf("MoveKurtoses.pdf", pointsize=9, family="CM Sans", width=4.2, height=3)
-plotManipulation(gains, kurts, c(0, 10, 6, 17), signals, "topleft", "Band Gain", "Spectral Kurtosis", bigMar)
+pdf("MoveKurtoses.pdf", pointsize=9, fonts=c("CM Roman", "CM Sans"), family="CM Sans", width=4.2, height=3)
+plotManipulation(gains, kurts, c(0, 10, 6, 17), signals, "topleft", "", "Spectral Kurtosis", bigMar)
+mtext(expression(italic(m)), side=1, line=3, family="CM Roman")
 dev.off()
 embed_fonts("MoveKurtoses.pdf")
 
