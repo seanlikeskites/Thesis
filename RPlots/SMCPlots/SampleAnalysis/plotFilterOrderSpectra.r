fdb59ee43e1c6602dd5b7c95d096b0e594790cbb
Sean Enderby <sean.enderby@gmail.com>
2017-06-02 14:13:53 +0100

    and the rings of peace were destroyed

--- RPlots/SMCPlots/SampleAnalysis/plotFilterOrderSpectra.r
+++ RPlots/SMCPlots/SampleAnalysis/plotFilterOrderSpectra.r
@@ -9,7 +9,7 @@ fir <- 20*log10(data$firSpec)
 
 pdf("CelloFilterOrderSpectra.pdf", pointsize=9, family="CM Sans", width=4.2, height=3)
 par(mar=c(4.1, 4, 0.8, 0.5), xaxs='i', yaxs='i')
-plot(freqs, iir, xlim=c(0, 10), ylim=c(-120, -20), xlab="Frequency (kHz)", ylab="Amplitude (dB)")
+plot(freqs, iir, type='n', xlim=c(0, 10), ylim=c(-120, -20), xlab="Frequency (kHz)", ylab="Amplitude (dB)")
 lines(freqs, iir, col="green")
 lines(freqs, fir, col="blue")
 box()
