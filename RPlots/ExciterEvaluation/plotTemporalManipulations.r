43445afab1d711434ac5ce8f3a7e7001ed1b9858
Sean Enderby <sean.enderby@gmail.com>
2017-06-08 09:11:34 +0100

    for through the millenia they strove for excellence

--- RPlots/ExciterEvaluation/plotTemporalManipulations.r
+++ RPlots/ExciterEvaluation/plotTemporalManipulations.r
@@ -28,8 +28,15 @@ plotTemporalManipulation <- function(t, sigs, envs, names, yspace)
 
 	for (i in 1:nSigs)
 	{
-		lines(t, sigs[[i]], col=adjustcolor(colours[i], alpha=0.4))
-		lines(t, envs[[i]], col=colours[i], lty=3, lwd=2)
+		if (is.null(envs))
+		{
+			lines(t, sigs[[i]], col=colours[i])
+		}
+		else
+		{
+			lines(t, sigs[[i]], col=adjustcolor(colours[i], alpha=0.4))
+			lines(t, envs[[i]], col=colours[i], lty=3, lwd=2)
+		}
 	}
 
 	legend("bottom", legend=names, col=colours[1:nSigs], lty=1)
@@ -46,14 +53,14 @@ embed_fonts("InfinitePeakClipping.pdf")
 cubed <- sig^3
 pdf("MultiplierTemporalEffects.pdf", pointsize=9, family="CM Sans", width=4.2, height=3)
 plotTemporalManipulation(t, list(sig, cubed), list(env, env^3),
-			 c("Original Signal", "Signal Cubed"), -1.5)
+			 c("Original Signal", "Cubed Signal"), -1.4)
 dev.off()
 embed_fonts("MultiplierTemporalEffects.pdf")
 
 cubeRoot <- sign(sig)*abs(sig)^(1/3)
 pdf("ExponentiationTemporalEffects.pdf", pointsize=9, family="CM Sans", width=4.2, height=3)
 plotTemporalManipulation(t, list(sig, cubeRoot), list(env, env^(1/3)),
-			 c("Original Signal", "Signal Cube Root"), -1.5)
+			 c("Original Signal", "Cube Rooted Signal"), -1.4)
 dev.off()
 embed_fonts("ExponentiationTemporalEffects.pdf")
 
@@ -74,3 +81,32 @@ pdf("IAPTemporalEffects.pdf", pointsize=9, family="CM Sans", width=4.2, height=3
 plotTemporalManipulation(t, list(sig, iap), list(env, env), c("Original Signal", "Third Order IAP"), -1.4)
 dev.off()
 embed_fonts("IAPTemporalEffects.pdf")
+
+# integrator
+integrator <- function(sig, k)
+{
+	l <- length(sig)
+	px <- 0
+	py <- 0
+	out <- array(0, l)
+
+	for (i in 1:l)
+	{
+		if ((sig[i] > 0) && (px <= 0))
+			out[i] = 0
+		else
+			out[i] = py + k*abs(sig[i])
+
+		px = sig[i]
+		py = out[i]
+	}
+
+	return(out)
+}
+
+int <- integrator(clipped, 4)
+int <- int / max(abs(int))
+pdf("IntegratorTemporalEffects.pdf", pointsize=9, family="CM Sans", width=4.2, height=3)
+plotTemporalManipulation(t, list(clipped, int), NULL, c("Original Signal", "Integrated Signal"), -1.4)
+dev.off()
+embed_fonts("IntegratorTemporalEffects.pdf")
