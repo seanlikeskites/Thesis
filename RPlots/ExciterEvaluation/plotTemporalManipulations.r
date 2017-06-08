32fb4118ec0168a076ef9e0af2359c0b82aba9a9
Sean Enderby <sean.enderby@gmail.com>
2017-06-08 08:41:25 +0100

    and lo they brought unity to the idols

--- RPlots/ExciterEvaluation/plotTemporalManipulations.r
+++ RPlots/ExciterEvaluation/plotTemporalManipulations.r
@@ -13,7 +13,12 @@ sig[(1:(2*fs)) + fs] <- sig[(1:(2*fs)) + fs] * up
 sig[(1:(2*fs)) + 4*fs] <- sig[(1:(2*fs)) + 4*fs] * rev(up)
 sig[(1:fs) + 6*fs] <- 0
 
-plotTemporalManipulation <- function(t, sigs, names, yspace)
+env <- array(0, length(sig))
+env[(1:(2*fs)) + fs] <- up
+env[(3*fs):(4*fs)] <- 1
+env[(1:(2*fs)) + 4*fs] <- rev(up)
+
+plotTemporalManipulation <- function(t, sigs, envs, names, yspace)
 {
 	par(mar=c(0.1, 0, 0, 0))
 	plot(t, sigs[[1]], type='n', axes=FALSE, ylim=c(yspace, 1))
@@ -23,7 +28,8 @@ plotTemporalManipulation <- function(t, sigs, names, yspace)
 
 	for (i in 1:nSigs)
 	{
-		lines(t, sigs[[i]], col=colours[i])
+		lines(t, sigs[[i]], col=adjustcolor(colours[i], alpha=0.4))
+		lines(t, envs[[i]], col=colours[i], lty=3, lwd=2)
 	}
 
 	legend("bottom", legend=names, col=colours[1:nSigs], lty=1)
@@ -32,29 +38,39 @@ plotTemporalManipulation <- function(t, sigs, names, yspace)
 # clipping
 clipped <- sign(sig)
 pdf("InfinitePeakClipping.pdf", pointsize=9, family="CM Sans", width=4.2, height=3)
-plotTemporalManipulation(t, list(sig, clipped), c("Original Signal", "Clipped Signal"), -1.4)
+plotTemporalManipulation(t, list(sig, clipped), list(env, sign(env)), c("Original Signal", "Clipped Signal"), -1.4)
 dev.off()
 embed_fonts("InfinitePeakClipping.pdf")
 
 # exp
 cubed <- sig^3
+pdf("MultiplierTemporalEffects.pdf", pointsize=9, family="CM Sans", width=4.2, height=3)
+plotTemporalManipulation(t, list(sig, cubed), list(env, env^3),
+			 c("Original Signal", "Signal Cubed"), -1.5)
+dev.off()
+embed_fonts("MultiplierTemporalEffects.pdf")
+
 cubeRoot <- sign(sig)*abs(sig)^(1/3)
 pdf("ExponentiationTemporalEffects.pdf", pointsize=9, family="CM Sans", width=4.2, height=3)
-plotTemporalManipulation(t, list(sig, cubed, cubeRoot), c("Original Signal", "Signal Cubed", "Signal Cube Root"), -1.5)
+plotTemporalManipulation(t, list(sig, cubeRoot), list(env, env^(1/3)),
+			 c("Original Signal", "Signal Cube Root"), -1.5)
 dev.off()
 embed_fonts("ExponentiationTemporalEffects.pdf")
 
 # ssb
 hilb <- hilbert(sig, f=fs)
 ssb <- Re(hilb^(3))
+ssb <- ssb / max(abs(ssb))
 pdf("SSBATemporalEffects.pdf", pointsize=9, family="CM Sans", width=4.2, height=3)
-plotTemporalManipulation(t, list(sig, ssb), c("Original Signal", "Third Order SSBA"), -1.4)
+plotTemporalManipulation(t, list(sig, ssb), list(env, env^3),
+			 c("Original Signal", "Third Order SSBA"), -1.4)
 dev.off()
 embed_fonts("SSBATemporalEffects.pdf")
 
 # iap
-iap <- abs(hilb)*cos(3*Arg(hilb));
+iap <- abs(hilb)*cos(3*Arg(hilb))
+iap <- iap / max(abs(iap))
 pdf("IAPTemporalEffects.pdf", pointsize=9, family="CM Sans", width=4.2, height=3)
-plotTemporalManipulation(t, list(sig, iap), c("Original Signal", "Third Order IAP"), -1.4)
+plotTemporalManipulation(t, list(sig, iap), list(env, env), c("Original Signal", "Third Order IAP"), -1.4)
 dev.off()
 embed_fonts("IAPTemporalEffects.pdf")
